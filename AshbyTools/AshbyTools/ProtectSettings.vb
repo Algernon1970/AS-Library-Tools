8d65097fff1ba4eec76bd5bb75073c01968e8869
PeterPage <p.page@ashbyschool.org.uk>
2016-06-22 10:39:24 +0100

    Included Protect Settings To Encrypt .config files

---
+++ AshbyTools/AshbyTools/ProtectSettings.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Configuration
+Module ProtectSettings
+
+    Private Sub protectSettings()
+        Dim config As Configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None)
+        config.ConnectionStrings.SectionInformation.ProtectSection(Nothing)
+        ' We must save the changes to the configuration file.
+        config.Save(ConfigurationSaveMode.Full, True)
+    End Sub
+End Module
